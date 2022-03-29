e6baf726a53ea1cb3dadac8165e614e012737c4f
Rapxidia <rapxidia96@gmail.com>
2022-03-28 21:13:44 -0600

    primer commit de inicio con header y hero

---
+++ src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import Hero from './Hero';
+
+const Layout = () => {
+	return (
+		<div className="Layout">
+			<Header />
+			<Hero/ >
+		</div>
+	);
+}
+
+export default Layout;
